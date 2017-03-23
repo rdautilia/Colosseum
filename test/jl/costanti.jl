7d23553fc41199fdeaa0ad6fcad2da06af88a0cf
Roberto D'Autilia <dautilia@gmail.com>
2017-03-23 12:15:04 +0100

    aggiornamento

---
+++ test/jl/costanti.jl
@@ -0,0 +1,7 @@
+
+\#MEMO: MODIFICARE IL 2N NEI FOR, CHE È INGUARDABILE
+\# INIZIALIZZO
+const N=1000 ::Int64                \# Il numero di pedoni
+const dt = 1.0 ::Float64               \# Il passo di integrazione
+const numero\_iterazioni = 500            \# Il numero di iterazioni della simulazione
+const diag = sqrt(2) ::Float64            \# diagonale
