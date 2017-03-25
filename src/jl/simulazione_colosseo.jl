2b599d81272f734305942088431ad88d58f58020
Roberto D'Autilia <dautilia@gmail.com>
2017-03-25 17:36:40 +0100

    versione con Statopedone

--- src/jl/simulazione_colosseo.jl
+++ src/jl/simulazione_colosseo.jl
@@ -17,6 +17,7 @@ const areacolosseo_coord = [314 256; 329 242; 359 223; 382 212; 419 204; 454 200
 523 211; 558 223; 597 245; 624 266; 659 306; 682 352; 688 403; 678 452; 651 444; 631 482; 
 600 507; 562 525; 520 533; 485 531; 446 524; 412 510; 374 490; 344 462; 314 420; 300 384; 
 300 351; 304 323; 312 299; 322 285] # L'earea interna
+const areacolosseo = ciclo_poligono(areacolosseo_coord)
 
 ##########################################################################
 
@@ -32,7 +33,7 @@ fonti = Font("../../fonts/kenvector_future.ttf") :: Font
 #########################################
 
 
-######### Funzione che formatta il poligono ciclicamente
+######### Funzione che formatta il poligono ciclicamente #####
 function ciclo_poligono(lista)
 	c=[]
 	for i=2:size(lista)[1]
@@ -48,8 +49,7 @@ function ciclo_poligono(lista)
 	cal=reshape(c,4,size(lista)[1])
 			return transpose(cal)
 end
-############################################################## NON CAPISCO PERCHE' DEBBA ANDARE QUI
-const areacolosseo = ciclo_poligono(areacolosseo_coord)
+##############################################################
 
 ###### DICTIONARY POLIGONI DEGLI EDIFICI ##################
 const edifici_coord = Dict{String, Array}(
