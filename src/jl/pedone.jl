b4edb01bd90dba1033564971f011bfe78854e391
Roberto D'Autilia <dautilia@gmail.com>
2017-03-31 09:43:46 +0200

    libreria pedone.jl: aggiunto il versore principale e quello reciproco

--- src/jl/pedone.jl
+++ src/jl/pedone.jl
@@ -12,9 +12,27 @@ end
 const STATOPEDONE_DEFAULT = Statopedone(0.1,0.1,0.1,0.1,0.1,0.1)
 const STATOPEDONE_ZERO    = Statopedone(-10.0,-10.0,0.0,0.0,0.0,0.0)
 
+## Calcola il versore principale, dalla posizione del pedone alla destinazione
+function versore_principale(pedone::Statopedone)
+	norm = sqrt((pedone.lax-pedone.ladestx)^2+(pedone.lay-pedone.ladesty)^2)
+	ex=(pedone.lax-pedone.ladestx)/norm
+	ey=(pedone.lay-pedone.ladesty)/norm
+	return  [ex, ey]
+end
+##########
 
-
-
+## Calcola il versore dal pedone j al pedone i
+function versore_reciproco(pedj::Statopedone,pedk::Statopedone)
+	norm = sqrt((pedj.lax-pedk.lax)^2+(pedj.lay-pedk.lay)^2)
+	if norm == 0
+		ex = 0.0
+		ey = 0.0
+	else
+		ex=(pedj.lax-pedk.lax)/norm
+		ey=(pedj.lay-pedk.lay)/norm
+	end
+	return  [ex, ey]
+end
 ### Aggiunge un pedone ##########
 function aggiungi_pedone()
 a = rand(1:N)
